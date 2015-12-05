baa039ec281f8fed40b8e9e46590b00a104c10ab
pdynowski <paul.dynowski@gmail.com>
2015-12-05 17:58:38 -0600

    Add photos and some scripting

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
-  document.getElementById("testarea").innerHTML = "HELLO THERE JADIAHGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+  var projects = [
+    {
+      project_image: "../imgs/test-image.png",
+      project_info: "A test image for the project"
+    },
+    {
+      project_image: "../imgs/test-image-2.png",
+      project_info: "A second test image for the project"
+    }
+  ];
+
+  document.getElementById("thumb-info").innerHTML = "Information regarding the project";
   console.log("loading");
 });
